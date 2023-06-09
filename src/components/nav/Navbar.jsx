046870b852ac9f03ace793607dcb0724e44e89b6
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-09 22:52:14 +0100

    feat: add montserrat font

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,12 +2,12 @@
 
 const Navbar = () => {
     return (
-      <nav className="w-full bg-[#FAFAFA] ">
+      <nav className="w-full bg-[#FAFAFA] font-lato">
         <div className="flex justify-between items-center py-4 w-[80%] mx-auto">
           <div>
-            <span>HolidayHaven</span>
+            <span className="font-bold text-dark text-2xl font-mont">HolidayHaven</span>
           </div>
-                <ul className="flex space-x-6 text-brown font-semibold">
+                <ul className="flex space-x-8 text-brown font-semibold items-center">
                     <li>Home</li>
                     <li>Rooms</li>
                     <li>Contact</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
       gray: "#8FCFFF",
       dark: "#333333",
     },
+    fontFamily: {
+      lato: ["Lato", 'sans-serif'],
+    mont: ['Montserrat', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
