f0607aace2a5ef5f0cef1d7474a46ba7253659ad
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-09 22:22:53 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      brown: "#B55B02",
+      gray: "#8FCFFF",
+      dark: "#333333",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
