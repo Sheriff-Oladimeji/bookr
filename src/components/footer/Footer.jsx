b74f8352cd20045c14d64ee58ea4e7254719205b
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-11 00:01:18 +0100

    feat: add hover state to footer icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,14 +4,14 @@ import { FaFacebookF } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-brown text-white py-8">
-      <div className="w-[90%] md:w-[80%] mx-auto">
+      <div className="w-[90%] md:w-[80%] mx-auto grid md:grid-cols-3">
         <div>
-                  <span className="font-bold text-2xl font-mont">Bookr</span>
-                  <div>
-                      <FaFacebookF/>
-                     <BsTwitter/>
-                    <BsInstagram/>
-                  </div>
+          <span className="font-bold text-2xl font-mont">Bookr</span>
+          <div className="flex gap-4 text-white  cursor-pointer">
+            <FaFacebookF size={20} className="hover:text-dark" />
+            <BsTwitter size={20} className="hover:text-dark" />
+            <BsInstagram size={20} className="hover:text-dark" />
+          </div>
         </div>
       </div>
     </footer>
