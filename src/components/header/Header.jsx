619592cf3fcdb028809fea5ad74deaa99d99082f
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-10 12:05:44 +0100

    feat: add bg to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,13 @@
 
 const Header = () => {
   return (
-    <header className="w-full h-screen">
-   <img src="" alt="" />   
- </header>
+    <header className="header font-lato">
+      <div className="flex flex-col justify-center items-center pt-32  w-[90%] mx-auto">
+        <h1 className="text-3xl  sm:text-4xl md:text-5xl mx-auto">
+          Welcome To The Place Where Luxury Meets Affordability
+        </h1>
+      </div>
+    </header>
   );
 }
 
