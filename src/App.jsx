1cdb3f2189872bfb925758650d23a9a8f5a62d8b
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-11 07:11:02 +0100

    fix: fix outlet not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
 import Home from "./Pages/home/Home";
 import Rooms from "./Pages/rooms/Rooms";
 import Contact from "./Pages/contact/Contact";
 import RootLayout from "./layout/RootLayout";
 import NotFound from "./Pages/404/NotFound";
-
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />}/>
+        <Route path="contact" element={<Contact />} />
+        <Route path="rooms" element={<Rooms />} />
+        <Route path="*" element={<NotFound/>} />
+         </Route>
+    )
+  );
   return (
-    <Router>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="rooms" element={<Rooms />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </RootLayout>
-    </Router>
+   <RouterProvider router={router}/>
   );
 };
 
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../components/nav/Navbar"
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/nav/Navbar";
 
 const RootLayout = () => {
   return (
-      <div>
-          <Navbar />
-          <main>
-              <Outlet/>
-          </main>
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
