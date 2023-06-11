34f6c6d6d199c76e8eed9c5cd95da18c603968ba
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-11 15:19:20 +0100

    fix: made changes to rooms component

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -2,8 +2,10 @@ import {BsPeople} from "react-icons/bs"
 import rooms from "../../data/rooms.json"
 const Rooms = () => {
   return (
-    <main className="mb-20 mt-36">
-      <h1 className="text-4xl text-dark md:text-6xl text-center mb-12">Our Rooms</h1>
+    <main className="mb-20 mt-36 font-mont">
+      <h1 className="text-4xl text-dark md:text-6xl text-center mb-12">
+        Our Rooms
+      </h1>
       <div className="w-[90%] md:w-[80%] mx-auto grid md:grid-cols-3 gap-8">
         {rooms.map((room, index) => (
           <div
@@ -19,9 +21,10 @@ const Rooms = () => {
               <h2 className="text-2xl md:text-xl font-bold text-dark">
                 {room.name}
               </h2>
-                    <span className="flex text-lg items-center gap-3 text-gray-600">
-                        <BsPeople/>
-                {room.guests}adult(s)and {room.children}child(ren)
+              <span className="flex text-lg items-center gap-3 text-gray-600">
+                <BsPeople />
+                {room.guests}adult(s)
+                {room.children && ` and ${room.children} child(ren)`}
               </span>
               <button className="border-2 border-brown p-2  w-[60%] md:w-[80%]  rounded-lg hover:bg-brown text-brown font-semibold hover:text-white">
                 Book now {room.price}
