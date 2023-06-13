ed96b2cfa87f9e1b3166c04aef517f78a2d24ae8
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-13 23:06:31 +0100

    fix: add your cart is empty

--- src/Pages/cart/Cart.jsx
+++ src/Pages/cart/Cart.jsx
@@ -1,22 +1,42 @@
 
-import { useSelector } from "react-redux";
+import { useSelector} from "react-redux";
+
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
 
+   const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+
   return (
-    <div>
+    <div className="mt-12 w-[90%] md:w-[80%] mx-auto">
       <h1>Cart Page</h1>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <p className="py-8 text-3xl">Your cart is empty.</p>
       ) : (
-        cartItems.map((item) => (
-          <div key={item.id}>
-            <h2>{item.name}</h2>
-            <img src={item.img} alt={item.name} />
-            <p>{item.price}</p>
+        <>
+          {cartItems.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between mt-12 items-center border-b"
+            >
+              <div className="flex items-center gap-6">
+                <img src={item.img} alt={item.name} className="w-[10%]" />
+                <h2>{item.name}</h2>
+              </div>
+              <p>{item.price}</p>
+              <button>Remove</button>
+            </div>
+          ))}
+            <div className="flex gap-4 items-center">
+              <p className="font-bold">Total price =</p>
+              <p>{totalPrice}</p>
+            </div>
+          <div className="w-[60%] md:w-[30%] mx-auto">
+            <button className="border-2 border-brown p-4  rounded-lg  mt-8 bg-brown font-semibold text-white text-center mb-4 w-full">
+              Checkout
+            </button>
           </div>
-        ))
+        </>
       )}
     </div>
   );
--- src/features/cart.js
+++ src/features/cart.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
-  cartItems: [],
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
 };
 
 const cartSlice = createSlice({
@@ -11,13 +12,11 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       state.cartItems.push(action.payload);
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+     
     },
   },
 });
 
-
-
-
-
 export const { addToCart } = cartSlice.actions;
-export default cartSlice.reducer
+export default cartSlice.reducer;
