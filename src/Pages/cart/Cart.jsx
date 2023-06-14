2c21d3997033bf81fada4929964feae168142e55
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-06-14 10:35:15 +0100

    feat: add delete item from cart

--- src/Pages/cart/Cart.jsx
+++ src/Pages/cart/Cart.jsx
@@ -1,14 +1,21 @@
 
 import { useSelector} from "react-redux";
-
+import { AiOutlineDelete } from "react-icons/ai"
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../features/cart";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
 
-   const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+    const dispatch = useDispatch();
+
+    const handleRemoveFromCart = (itemId) => {
+      dispatch(removeFromCart(itemId));
+    };
 
   return (
-    <div className="mt-12 w-[90%] md:w-[80%] mx-auto">
+    <div className="mt-12 w-[90%] md:w-[80%] mx-auto font-mont">
       <h1>Cart Page</h1>
       {cartItems.length === 0 ? (
         <p className="py-8 text-3xl">Your cart is empty.</p>
@@ -21,16 +28,18 @@ const Cart = () => {
             >
               <div className="flex items-center gap-6">
                 <img src={item.img} alt={item.name} className="w-[10%]" />
-                <h2>{item.name}</h2>
+                <h2 className="font-bold">{item.name}</h2>
               </div>
               <p>${item.price}</p>
-              <button>Remove</button>
+              <button onClick={() => handleRemoveFromCart(item.id)}>
+                <AiOutlineDelete size={25} />
+              </button>
             </div>
           ))}
-            <div className="flex gap-4 items-center">
-              <p className="font-bold">Total price =</p>
-              <p>${totalPrice}</p>
-            </div>
+          <div className="flex gap-4 items-center mt-4">
+            <p className="font-bold">Total price =</p>
+            <p>${totalPrice}</p>
+          </div>
           <div className="w-[60%] md:w-[30%] mx-auto">
             <button className="border-2 border-brown p-4  rounded-lg  mt-8 bg-brown font-semibold text-white text-center mb-4 w-full">
               Checkout
--- src/features/cart.js
+++ src/features/cart.js
@@ -13,10 +13,14 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       state.cartItems.push(action.payload);
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
-     
+    },
+    removeFromCart: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
