94e92fa485e26687189f43367207acf43c5fb630
Sheriff  Oladimeji <121828342+Sheriff-Oladimeji@users.noreply.github.com>
2023-06-14 11:00:09 +0100

    Update Rooms.jsx

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -32,7 +32,7 @@ const Rooms = () => {
                 to={`/room/${room.id}`}
                 className="border-2 border-brown p-2 w-[60%] md:w-[80%] rounded-lg hover:bg-brown text-brown font-semibold hover:text-white text-center"
               >
-                Book now {room.price}
+                Book now ${room.price}
               </Link>
             </div>
           </div>
